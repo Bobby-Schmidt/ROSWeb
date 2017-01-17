55ed1df8a14400ba26f7d283ade22c5ff5885321
Bobby-Schmidt <bcschmidt91@gmail.com>
2017-01-17 12:10:31 -0600

    Code snippet
    - Added example POST request to management.js

--- static/js/management.js
+++ static/js/management.js
@@ -2,7 +2,7 @@
 * @Author: bobby
 * @Date:   2017-01-17 11:41:07
 * @Last Modified by:   Bobby-Schmidt
-* @Last Modified time: 2017-01-17 11:57:56
+* @Last Modified time: 2017-01-17 12:10:10
 */
 
 function populateWorkspaces() {
@@ -16,6 +16,24 @@ function populatePackages() {
 	var selectedWorkspaces = $('#selectWorkspaces').find("option:selected");
 
 	console.log('selectedWorkspaces: ', selectedWorkspaces);
+
+	// $.ajax({
+	// 	type: "POST",
+	// 	// url: "http://192.168.0.16:5000/players/list",
+	// 	url: "http://127.0.0.1:5000/players/list",
+	// 	// url: "http://10.10.55.18:5000/games/list",
+	// 	data: JSON.stringify(trendRequestModel),
+	// 	// "url": "http://192.168.1.14:5000/teams-list",
+	// 	dataType: 'json',
+	// 	success: function(response) {
+	// 		var jsonData = response;
+	// 		initPlot(jsonData);
+	// 	},
+	// 	failure: function() {
+	// 		console.log('POST Failed');
+	// 	}
+	// });
+
 }
 
 
